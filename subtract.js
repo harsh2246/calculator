75424d37982d5d5de435b7f55b35ece8a0b619b7
Harsh Mangal <harsh.mangal@itilite.in>
2022-09-28 14:52:27 +0530

    subtract logic applied

---
+++ subtract.js
@@ -0,0 +1,7 @@
+function add(){
+
+num1=document.getElementyId("firstno");
+num2=document.getElementbyId("secondno");
+var result=parseInt(num1)-parseInt(num2)
+document.getElementbyId("result")=result
+}
